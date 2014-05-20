c9aa754de41214e0aa59f727e11d71ffc6edf22b
Eduardo Cereto <eduardocereto@gmail.com>
2014-05-20 02:25:28 -0300

    Use soft tabs

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 GA_WEBPROPERTY = 'UA-25868527-5'
 
 def sendMeasurementProtocolHit(params):
-	url = 'http://www.google-analytics.com/collect'
-	values = params
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
-	response = urllib2.urlopen(req)
+  url = 'http://www.google-analytics.com/collect'
+  values = params
+  data = urllib.urlencode(values)
+  req = urllib2.Request(url, data)
+  response = urllib2.urlopen(req)
 
 def makeTrans():
-	return True
+  return True
 
 class MainHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -33,30 +33,30 @@ class MainHandler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class HomePage(MainHandler):
-	def get(self):
-		self.render('home.html')
+  def get(self):
+    self.render('home.html')
 
 class ProcessTransactions(MainHandler):
-	def post(self):
-		transactionSuccessful = makeTrans()
-		if(transactionSuccessful):
-			clientId = self.request.get('clientId')
-			if(clientId == ''):
-				clientId = str(uuid.uuid4())
-				cd4 = 'server side'
-			else:
-				cd4 = 'analytics.js'
-			ip = self.request.remote_addr
-			userAgent = str(self.request.headers['User-Agent'])
-			params = dict(v = '1', 
-						  tid = GA_WEBPROPERTY,
-						  cid = clientId,
-						  uip = ip,
-						  ua = userAgent,
-						  dp = '/transaction-successful',
-						  cd4 = cd4			  
-						  )
-			sendMeasurementProtocolHit(params)
+  def post(self):
+    transactionSuccessful = makeTrans()
+    if(transactionSuccessful):
+      clientId = self.request.get('clientId')
+      if(clientId == ''):
+        clientId = str(uuid.uuid4())
+        cd4 = 'server side'
+      else:
+        cd4 = 'analytics.js'
+      ip = self.request.remote_addr
+      userAgent = str(self.request.headers['User-Agent'])
+      params = dict(v = '1', 
+              tid = GA_WEBPROPERTY,
+              cid = clientId,
+              uip = ip,
+              ua = userAgent,
+              dp = '/transaction-successful',
+              cd4 = cd4
+              )
+      sendMeasurementProtocolHit(params)
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
